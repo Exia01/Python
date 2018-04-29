f261e59429ff920fab145ee542393a1986878a11
Jose <35702950+Exia01@users.noreply.github.com>
2018-04-29 19:45:18 -0400

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,39 @@
+# print("Hello World")
+# x = ("Hello Python")
+# print(x)
+# y = 42
+# print(y)
+
+# name = "Zen"
+# print("My name is", name)
+
+# name = "Zen"
+# print("My name is " + name)
+
+# numb = 1
+# print(numb)
+# nameNumb = "Hello"
+# # print("Hello" + numb)
+# print("Hello", numb)
+
+# first_name = "Zen"
+# last_name = "Coder"
+# print("My name is {} {}".format(first_name, last_name))
+# ninjas = ['Rozen', 'KB', 'Oliver']
+# my_list = ['4', ['list', 'in', 'a', 'list'], 987]
+# empty_list = []
+# print(my_list[1][1], my_list[0])
+# age = 15
+# if age >= 18:
+#     print('Legal age')
+# else:
+#     print('You are so young!')
+# for val in "string":
+#     if val == "i":
+#         break
+#     print(val)
+i = 0
+for val in "string":
+    if val == "i":
+        continue
+print(val)
