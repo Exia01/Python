1a1fa7c13706d67569226700454d31b7d3582500
Jose <sixgg4@gmail.com>
2019-01-06 16:33:38 -0500

    Changed cart views, added BillingProfileManager

--- Django/Dev/Ecommerce/src/apps/billing/models.py
+++ Django/Dev/Ecommerce/src/apps/billing/models.py
@@ -2,28 +2,28 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 # from django.core.urlresolvers import reverse
-# from accounts.models import GuestEmail
+from ..accounts.models import GuestEmail
 User = settings.AUTH_USER_MODEL
 
 
-# class BillingProfileManager(models.Manager):
-#     def new_or_get(self, request):
-#         user = request.user
-#         guest_email_id = request.session.get('guest_email_id')
-#         created = False
-#         obj = None
-#         if user.is_authenticated():
-#             'logged in user checkout; remember payment stuff'
-#             obj, created = self.model.objects.get_or_create(
-#                 user=user, email=user.email)
-#         elif guest_email_id is not None:
-#             'guest user checkout; auto reloads payment stuff'
-#             guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
-#             obj, created = self.model.objects.get_or_create(
-#                 email=guest_email_obj.email)
-#         else:
-#             pass
-#         return obj, created
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        user = request.user
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+        if user.is_authenticated:
+            #'logged in user checkout; remember payment stuff'
+            obj, created = self.model.objects.get_or_create(
+                user=user, email=user.email)
+        elif guest_email_id is not None:
+            #'guest user checkout; auto reloads payment stuff'
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(
+                email=guest_email_obj.email)
+        else:
+            pass
+        return obj, created
 
 
 class BillingProfile(models.Model):
@@ -32,11 +32,11 @@ class BillingProfile(models.Model):
     email = models.EmailField()
     active = models.BooleanField(default=True)
     update = models.DateTimeField(auto_now=True)
-    # timestamp   = models.DateTimeField(auto_now_add=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
     # customer_id = models.CharField(max_length=120, null=True, blank=True)
     # customer_id in Stripe or Braintree
 
-    # objects = BillingProfileManager()
+    objects = BillingProfileManager()
 
     def __str__(self):
         return self.email
