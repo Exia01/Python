a0fbc01f1244265496793258d1a8f38e28423ab9
Jose <sixgg4@gmail.com>
2019-02-12 20:47:22 -0500

    scope and key-word-args

--- Self-Learning/back_to_python/functions/basics/functions_101.py
+++ Self-Learning/back_to_python/functions/basics/functions_101.py
@@ -1,14 +1,23 @@
+""" 
+A parameter is a variable in a method definition
+Argument is the actual value of the variable or "Parameter" that gets passed into the function
+"""
+
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo']
 colors.append('violet')
 print(colors)
 
 # .append, print, .clear these are all built-in function
 
-#defining a function
+# defining a function
+
+
 def say_hi():
     print('Hi!')
 
+
 say_hi()
+
 
 def sing_happy_birthday():
     print('Happy Birthday To You')
@@ -16,14 +25,31 @@ def sing_happy_birthday():
     print('Happy Birthday Dear You')
     print('Happy Birthday To You')
 
+
 print(sing_happy_birthday())
 
+# passing arguments
+
+
+def sing_happy_birthday_2(name):
+    print('Happy Birthday To You')
+    print('Happy Birthday To You')
+    print(f'Happy Birthday Dear {name}')
+    print('Happy Birthday To You')
+
+
+sing_happy_birthday_2("Timmy")
+
 num = 50
+
+
 def generate_evens(num):
     total = [i for i in range(0, num, 2)]
     return total
 
+
 print(generate_evens(num))
 
+
 def generate_evens2():
-    return [x for x in range(1,50) if x%2 == 0]
+    return [x for x in range(1, 50) if x % 2 == 0]
---
+++ Self-Learning/back_to_python/functions/basics/key-word-args.py
@@ -0,0 +1,19 @@
+def exponent(num, power=2):
+	return num ** power
+
+# Order doesn't matter anymore, if we use key word arguments:
+print(exponent(num=2,power=3)) #8
+print(exponent(power=3, num=2)) #8
+
+# Without keywords args, order still matters:
+print(exponent(3,4)) #81
+print(exponent(4,3)) #64
+
+
+#Another sample, 
+def full_name(first, last): #setting parameter
+	return f'Your name is {first} {last}'
+
+print(full_name(first='Jose', last="Gonzalez")) 
+print(full_name(last="Gonzalez", first='Jose'))  # we can also change the order of arguments
+
