5ff920dbc2da250d17131e80eea4227c2b1bff20
Jose <sixgg4@gmail.com>
2019-03-27 20:11:24 -0400

    completed authors scrapeper

--- Self-Learning/_Web_Scraping/projects/scraping_quotes/authors_scrapper.py
+++ Self-Learning/_Web_Scraping/projects/scraping_quotes/authors_scrapper.py
@@ -2,6 +2,7 @@ import requests
 from csv import DictReader, DictWriter
 from bs4 import BeautifulSoup
 from pathlib import Path
+from  word_splitter import split_into_sentences
 # custom packages
 
 data_folder = Path(
@@ -29,25 +30,30 @@ with open(quotes_file, 'r', encoding='utf-8-sig') as f:
 
 
 with open(authors_bio, 'w', encoding='utf-8-sig') as csv_file:
-    # headers = ("name", "dob_location", "trivia_info")
-    # csv_writer = DictWriter(csv_file, fieldnames=headers, lineterminator="\n")
-    # csv_writer.writeheader()
+    headers = ("name", "dob_location", "trivia_question")
+    csv_writer = DictWriter(csv_file, fieldnames=headers, lineterminator="\n")
+    csv_writer.writeheader()
     for author in authors_list:
         author_name = author.get("author")
-        if authors_dictionary.get(author_name) == 0: #true if 1
+        print(author_name)
+        if authors_dictionary.get(author_name) == 0:  #true if 1
+            name = author_name.split()[0]
             site = author.get("bio_link")
-            
             data = requests.get(site)
             soup = BeautifulSoup(data.text, "html.parser")
-            bod = soup.find("div", {"class": "author-details"}).find_next().find_next().get_text().split(":")[1][1::]
-            trivia = soup.find("div", {"class":"author-description"}).get_text().strip("\n")
-            print(trivia)
-            break
-            # csv_writer.writerow({
-                # "name": author_name,
-                # "dob_location"
-
-            # })
+            dob = soup.find("div", {"class": "author-details"}).find_next().find_next().get_text().split(":")[1][1::]
+            trivia = soup.find("div", {"class": "author-description"}).get_text()
+            trivia = split_into_sentences(trivia)
+            trivia = list(filter(lambda word: "." != word, trivia))
+            # trivia_questions = list(filter(lambda word: name not in word, trivia))[10::30]
+            question = list(filter(lambda word:name not in word, trivia))
+            question = question[2]
+            csv_writer.writerow({
+                "name": author_name,
+                "dob_location":dob,
+                "trivia_question":question
+            })
+            authors_dictionary[author_name] = 1
 
 
 # print(authors_dictionary)
